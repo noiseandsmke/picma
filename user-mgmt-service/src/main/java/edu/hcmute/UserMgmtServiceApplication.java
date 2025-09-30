2341b513d89d295995bba2317754003a84e70390
noiseandsmke <emailaccanhbg11@gmail.com>
2025-09-30 23:12:31 +0700

    init user management service

---
+++ user-mgmt-service/src/main/java/edu/hcmute/UserMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMgmtServiceApplication.class, args);
+    }
+
+}
