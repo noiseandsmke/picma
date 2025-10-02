c83fa97997bcb0456e3643156ecb68b9d32fc584
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-03 00:19:31 +0700

    add get members from a group

--- user-mgmt-service/src/main/java/edu/hcmute/outbound/UserOutboundApi.java
+++ user-mgmt-service/src/main/java/edu/hcmute/outbound/UserOutboundApi.java
@@ -27,56 +27,6 @@ public class UserOutboundApi {
         this.restTemplate = restTemplate;
     }
 
-    public List<User> getAllUsers(String accessToken) {
-        log.info("Get Users :: API = {}", picma_users_api);
-        MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
-        headersMap.add("Authorization", "Bearer " + accessToken);
-        HttpEntity<String> entity = new HttpEntity<>(headersMap);
-        try {
-            ResponseEntity<?> resEntity = restTemplate.exchange(picma_users_api, HttpMethod.GET, entity, Object.class);
-            log.info("Get Users :: Status code = {}", resEntity.getStatusCode().value());
-            if (resEntity.getStatusCode().is2xxSuccessful()) {
-                List<User> userListRes = (List<User>) resEntity.getBody();
-                log.info("Get Users :: NO. users = {}", userListRes != null ? userListRes.size() : 0);
-                return userListRes;
-            } else {
-                throw new RuntimeException("HttpStatus code: " + resEntity.getStatusCode().value());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<User> getAllPropertyOwners(String accessToken) {
-        log.info("Get POs :: API = {}", picma_groups_api);
-        picma_groups_api = picma_groups_api + "/" + picma_group_prop_owners + "/members";
-        log.info("Get POs :: API = {}", picma_groups_api);
-        MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
-        headersMap.add("Authorization", "Bearer " + accessToken);
-        HttpEntity<String> entity = new HttpEntity<>(headersMap);
-        try {
-            ResponseEntity<?> resEntity = restTemplate.exchange(picma_groups_api, HttpMethod.GET, entity, Object.class);
-            log.info("Get POs :: Status code = {}", resEntity.getStatusCode().value());
-            if (resEntity.getStatusCode().is2xxSuccessful()) {
-                List<User> userListRes = (List<User>) resEntity.getBody();
-                log.info("Get POs :: NO. users = {}", userListRes != null ? userListRes.size() : 0);
-                return userListRes;
-            } else {
-                throw new RuntimeException("HttpStatus code: " + resEntity.getStatusCode().value());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<User> getAllAgents() {
-        return null;
-    }
-
-    public List<User> getAllBrokers() {
-        return null;
-    }
-
     public User createUser(User user, String accessToken) {
         MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
         headersMap.add("Authorization", "Bearer " + accessToken);
@@ -92,19 +42,20 @@ public class UserOutboundApi {
                     log.info("Location = {}", headersValue);
                     String userId = headersValue.replace(picma_users_api + "/", "");
                     log.info("User ID = {}", userId);
-                    String groupId = null;
+                    String groupId;
                     if (!StringUtils.hasLength(user.getGroupId())) {
-                        log.info("Property Owners group ID = {}", groupId);
                         groupId = picma_group_prop_owners;
+                        log.info("Property Owners group ID = {}", groupId);
                     } else {
-                        log.info("Other group ID = {}", groupId);
                         groupId = user.getGroupId();
+                        log.info("Other group ID = {}", groupId);
                     }
                     boolean isProvisioned = provisioningUser(userId, groupId, accessToken);
                     if (isProvisioned) {
                         user.setUserId(userId);
                         user.setGroupId(groupId);
                     } else {
+                        // delete user
                     }
                 }
             } else {
@@ -148,5 +99,55 @@ public class UserOutboundApi {
             deprovisioned = true;
         }
         return deprovisioned;
+    }
+
+    public List<User> getAllUsers(String accessToken) {
+        log.info("Get Users :: API = {}", picma_users_api);
+        MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
+        headersMap.add("Authorization", "Bearer " + accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headersMap);
+        try {
+            ResponseEntity<?> resEntity = restTemplate.exchange(picma_users_api, HttpMethod.GET, entity, Object.class);
+            log.info("Get Users :: Status code = {}", resEntity.getStatusCode().value());
+            if (resEntity.getStatusCode().is2xxSuccessful()) {
+                List<User> userListRes = (List<User>) resEntity.getBody();
+                log.info("Get Users :: NO. users = {}", userListRes != null ? userListRes.size() : 0);
+                return userListRes;
+            } else {
+                throw new RuntimeException("HttpStatus code: " + resEntity.getStatusCode().value());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> getAllMembersOfGroup(String groupId, String accessToken) {
+        log.info("Get POs :: API = {}", picma_groups_api);
+        picma_groups_api = picma_groups_api + "/" + groupId + "/members";
+        log.info("Get POs :: API = {}", picma_groups_api);
+        MultiValueMap<String, String> headersMap = new LinkedMultiValueMap<>();
+        headersMap.add("Authorization", "Bearer " + accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headersMap);
+        try {
+            ResponseEntity<?> resEntity = restTemplate.exchange(picma_groups_api, HttpMethod.GET, entity, Object.class);
+            log.info("Get POs :: Status code = {}", resEntity.getStatusCode().value());
+            if (resEntity.getStatusCode().is2xxSuccessful()) {
+                List<User> userListRes = (List<User>) resEntity.getBody();
+                log.info("Get POs :: NO. users = {}", userListRes != null ? userListRes.size() : 0);
+                return userListRes;
+            } else {
+                throw new RuntimeException("HttpStatus code: " + resEntity.getStatusCode().value());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> getAllAgents() {
+        return null;
+    }
+
+    public List<User> getAllBrokers() {
+        return null;
     }
 }
