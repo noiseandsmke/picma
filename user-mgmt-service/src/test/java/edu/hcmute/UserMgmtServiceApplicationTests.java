894e7591e19b7dbb8d98b07264406d50ef0d87eb
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-03 00:20:09 +0700

    test create specific user (agent/broker/property owner)

--- user-mgmt-service/src/test/java/edu/hcmute/UserMgmtServiceApplicationTests.java
+++ user-mgmt-service/src/test/java/edu/hcmute/UserMgmtServiceApplicationTests.java
@@ -2,6 +2,7 @@ package edu.hcmute;
 
 import edu.hcmute.models.User;
 import edu.hcmute.outbound.UserOutboundApi;
+import org.assertj.core.util.DateUtil;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserMgmtServiceApplicationTests {
     static User newUser = null;
-    String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0S2FKVEdKRWxMS0N0ZHc1SVFrOEJ6OGRCd2tmQmVnR3lEd1NZVEhOWFpJIn0.eyJleHAiOjE3NTk0MjA2MzEsImlhdCI6MTc1OTQxODgzMSwianRpIjoidHJydGNjOjllNzJkNzg3LTMwMWQtZTk2Ny0wNjdkLWIxMjg1MGIxYWU1YSIsImlzcyI6Imh0dHA6Ly8xMjcuMC4wLjE6ODA4MS9yZWFsbXMvcGljbWEiLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiYTk1NGZlNDgtYWYxNy00MjcwLWE0NWUtNjQ4ZmY3Nzc5YmZlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYmFvdmlldC1pbnN1cmVyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXBpY21hIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJyZWFsbS1tYW5hZ2VtZW50Ijp7InJvbGVzIjpbIm1hbmFnZS11c2VycyJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SG9zdCI6IjE3Mi4xOC4wLjEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1iYW92aWV0LWluc3VyZXIiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjE4LjAuMSIsImNsaWVudF9pZCI6ImJhb3ZpZXQtaW5zdXJlciJ9.HmPchNc-2yThziVZTWCvh-F47pzqGNznqWLawDpQk-4WPrPfIS0koCmAD5VM5s9_-Bnuq_mkaLOfdCTARgi67l5ASxgWtYXkZxupj5e_r8bnE0qXyRqLRQEUZL089VGdBXJW9tl_Y71t3xN9B9471RUfS1VgJYaI62AbFni2TN-Qn7tAQDUl2H98rHrUhQlq-e9zTptt9DqtMXuCAOyBmWcEzeU8-XK5_azp-Jy_M8Xr-IS9aMKbf3l_8xpxYNanfPbZa0NJoAF3xs492vgumEEs_hEesTjGG_yM9W35fktAhamRfJJOkgu8yVG3gPdPecugaDfqrg5kvpphwyBHtw";
+    static String token = null;
+    static String propertyOwnerGroupId = null;
+    static String brokersGroupId = null;
+    static String agentsGroupId = null;
     @Autowired
     private UserOutboundApi userOutboundApi;
 
@@ -27,6 +31,10 @@ class UserMgmtServiceApplicationTests {
                 .email("capybara@lol.com")
                 .username("capybara")
                 .build();
+        token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0S2FKVEdKRWxMS0N0ZHc1SVFrOEJ6OGRCd2tmQmVnR3lEd1NZVEhOWFpJIn0.eyJleHAiOjE3NTk0MjU2NjksImlhdCI6MTc1OTQyMzg2OSwianRpIjoidHJydGNjOjU1NjRjNjI4LWU2ZTYtZTBlNy0wY2EyLTgwMjQxZjM3ZjE4NSIsImlzcyI6Imh0dHA6Ly8xMjcuMC4wLjE6ODA4MS9yZWFsbXMvcGljbWEiLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiYTk1NGZlNDgtYWYxNy00MjcwLWE0NWUtNjQ4ZmY3Nzc5YmZlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYmFvdmlldC1pbnN1cmVyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXBpY21hIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJyZWFsbS1tYW5hZ2VtZW50Ijp7InJvbGVzIjpbIm1hbmFnZS11c2VycyJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SG9zdCI6IjE3Mi4xOC4wLjEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1iYW92aWV0LWluc3VyZXIiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjE4LjAuMSIsImNsaWVudF9pZCI6ImJhb3ZpZXQtaW5zdXJlciJ9.mcqKWN2tEy3k48VFk-IAIwhzdnk0Y2pk-uwNDG5szL_q3QMz2JoMlhfMTkJREsotH5ewGnCur0OzRNfWe9SH-dQJGQfp-0luT3v6cwYrTYWW_202wbXXIWP6lxCp0PJl3y4ebr0qhgArCf1KBMyVd7fUkat5ga1It244C0c2E6IohKCr6htd0i124I3Hh__28MiXae4QTaIKrNnC-tjYy5gKuTKsA6cCYicdSvxJWU1fj-gYTjO7Hcf6SKt_paiIjO4x_bWF6TzODwgRBeCgIQA8qWru9U60uqbWRehmwU__hF2wU1TppoYSXFOwoiKToaQb71k41mWo_VYyqYiZbw";
+        agentsGroupId = "1a142694-4296-41fa-a475-8f384c84e373";
+        brokersGroupId = "3a9546ac-5c1d-4323-92a0-f858a6619304";
+        propertyOwnerGroupId = "163e8a2c-8788-4bfc-bff8-e0d349bc9ac2";
     }
 
     @Test
@@ -39,20 +47,62 @@ class UserMgmtServiceApplicationTests {
     }
 
     @Test
-    @Order(2)
-    public void testGetAllPropertyOwners() {
-        List<User> propertyOwners = userOutboundApi.getAllPropertyOwners(token);
-        assertThat(propertyOwners);
-        Assertions.assertFalse(propertyOwners.isEmpty());
-    }
-
-    @Test
     @Order(1)
-    public void testCreateUser() {
+    public void testCreatePropertyOwnerUser() {
+        newUser.setUsername(newUser.getUsername() + DateUtil.now().getTime());
+        newUser.setEmail(newUser.getEmail() + DateUtil.now().getTime());
         newUser = userOutboundApi.createUser(newUser, token);
         assertThat(newUser);
         assertThat(newUser.getUserId());
         assertThat(newUser.getGroupId());
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateBrokerUser() {
+        newUser.setUsername(newUser.getUsername() + DateUtil.now().getTime());
+        newUser.setEmail(newUser.getEmail() + DateUtil.now().getTime());
+        newUser.setGroupId(brokersGroupId);
+        newUser = userOutboundApi.createUser(newUser, token);
+        assertThat(newUser);
+        assertThat(newUser.getUserId());
+        assertThat(newUser.getGroupId());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreateAgentUser() {
+        newUser.setUsername(newUser.getUsername() + DateUtil.now().getTime());
+        newUser.setEmail(newUser.getEmail() + DateUtil.now().getTime());
+        newUser.setGroupId(agentsGroupId);
+        newUser = userOutboundApi.createUser(newUser, token);
+        assertThat(newUser);
+        assertThat(newUser.getUserId());
+        assertThat(newUser.getGroupId());
+    }
+
+    @Test
+    @Order(4)
+    public void testGetAllPropertyOwnerMembers() {
+        List<User> membersOfGroup = userOutboundApi.getAllMembersOfGroup(propertyOwnerGroupId, token);
+        assertThat(membersOfGroup);
+        Assertions.assertFalse(membersOfGroup.isEmpty());
+    }
+
+    @Test
+    @Order(5)
+    public void testGetAllBrokerMembers() {
+        List<User> membersOfGroup = userOutboundApi.getAllMembersOfGroup(brokersGroupId, token);
+        assertThat(membersOfGroup);
+        Assertions.assertFalse(membersOfGroup.isEmpty());
+    }
+
+    @Test
+    @Order(6)
+    public void testGetAllAgentMembers() {
+        List<User> membersOfGroup = userOutboundApi.getAllMembersOfGroup(agentsGroupId, token);
+        assertThat(membersOfGroup);
+        Assertions.assertFalse(membersOfGroup.isEmpty());
     }
 
     //    @Test
