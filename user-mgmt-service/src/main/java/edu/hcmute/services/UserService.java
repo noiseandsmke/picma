f730e1b859e3c4d9df4bc5175187363ce9d70db6
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-10 08:40:49 +0700

    change type of createUser from User to UserBean

--- user-mgmt-service/src/main/java/edu/hcmute/services/UserService.java
+++ user-mgmt-service/src/main/java/edu/hcmute/services/UserService.java
@@ -1,12 +1,11 @@
 package edu.hcmute.services;
 
 import edu.hcmute.beans.UserBean;
-import edu.hcmute.models.User;
 
 import java.util.List;
 
 public interface UserService {
-    User createUser(UserBean userBean, String accessToken);
+    UserBean createUser(UserBean userBean, String accessToken);
 
     List<UserBean> getAllUsers(String accessToken);
 
