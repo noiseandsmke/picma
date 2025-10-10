b0708d1ce97231f497065730f6936cb0777ac572
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-10 08:40:19 +0700

    add model mapper configuration

--- user-mgmt-service/src/main/java/edu/hcmute/config/AppConfig.java
+++ user-mgmt-service/src/main/java/edu/hcmute/config/AppConfig.java
@@ -1,6 +1,8 @@
 package edu.hcmute.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,5 +17,12 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 }
