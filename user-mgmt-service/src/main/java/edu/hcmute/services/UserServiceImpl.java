68eba677560fa93a9f0efbbd8426602571ec5d60
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-10 08:47:36 +0700

    implement services for user

--- user-mgmt-service/src/main/java/edu/hcmute/services/UserServiceImpl.java
+++ user-mgmt-service/src/main/java/edu/hcmute/services/UserServiceImpl.java
@@ -2,24 +2,60 @@ package edu.hcmute.services;
 
 import edu.hcmute.beans.UserBean;
 import edu.hcmute.models.User;
+import edu.hcmute.outbound.UserOutboundApi;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
+    private UserOutboundApi userOutboundApi;
+    private ModelMapper modelMapper;
+
+    public UserServiceImpl(UserOutboundApi userOutboundApi, ModelMapper modelMapper) {
+        this.userOutboundApi = userOutboundApi;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
-    public User createUser(UserBean userBean, String accessToken) {
-        return null;
+    public UserBean createUser(UserBean userBean, String accessToken) {
+        User user = modelMapper.map(userBean, User.class);
+        log.info("User :: {}", user.toString());
+        user = userOutboundApi.createUser(user, accessToken);
+        userBean = modelMapper.map(user, UserBean.class);
+        return userBean;
     }
 
     @Override
     public List<UserBean> getAllUsers(String accessToken) {
-        return List.of();
+        List<User> userList = userOutboundApi.getAllUsers(accessToken);
+        if (!CollectionUtils.isEmpty(userList)) {
+            List<UserBean> uiUserList = new ArrayList<>();
+            userList.stream().forEach((user) -> {
+                uiUserList.add(modelMapper.map(user, UserBean.class));
+            });
+            return uiUserList;
+        } else {
+            throw new RuntimeException("No users found");
+        }
     }
 
     @Override
     public List<UserBean> getAllMembersOfGroup(String groupId, String accessToken) {
-        return List.of();
+        List<User> userList = userOutboundApi.getAllMembersOfGroup(groupId, accessToken);
+        if (!CollectionUtils.isEmpty(userList)) {
+            List<UserBean> uiUserList = new ArrayList<>();
+            userList.stream().forEach((user) -> {
+                uiUserList.add(modelMapper.map(user, UserBean.class));
+            });
+            return uiUserList;
+        } else {
+            throw new RuntimeException("No users found");
+        }
     }
 }
