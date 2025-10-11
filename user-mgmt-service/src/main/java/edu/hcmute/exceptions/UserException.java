838161ad1a9c47cce1df7fe34c526af4c45cb03b
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-11 15:28:32 +0700

    add exceptions

---
+++ user-mgmt-service/src/main/java/edu/hcmute/exceptions/UserException.java
@@ -0,0 +1,17 @@
+package edu.hcmute.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.Serial;
+
+@Getter
+//@Builder
+@AllArgsConstructor
+public class UserException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = -8761721031457177925L;
+
+    private String errorMessage;
+    private int errorCode;
+}
---
+++ user-mgmt-service/src/main/java/edu/hcmute/exceptions/UserRestException.java
@@ -0,0 +1,15 @@
+package edu.hcmute.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.Serial;
+
+@Getter
+//@Builder
+@AllArgsConstructor
+public class UserRestException extends Exception {
+    @Serial
+    private static final long serialVersionUID = -8761721031457177925L;
+    private Throwable cause;
+}
