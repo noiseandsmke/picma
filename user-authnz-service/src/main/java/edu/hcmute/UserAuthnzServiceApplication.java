e903c6d47f29e3b7a2b1bbf7e816dba46d059223
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-12 23:22:52 +0700

    init the authnz service

---
+++ user-authnz-service/src/main/java/edu/hcmute/UserAuthnzServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthnzServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthnzServiceApplication.class, args);
+    }
+
+}
