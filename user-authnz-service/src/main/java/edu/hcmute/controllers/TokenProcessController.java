f19efd58d3373d71a5bbec5c4572034f4df5ca3c
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-13 18:30:01 +0700

    add generateToken

--- user-authnz-service/src/main/java/edu/hcmute/controllers/TokenProcessController.java
+++ user-authnz-service/src/main/java/edu/hcmute/controllers/TokenProcessController.java
@@ -2,7 +2,10 @@ package edu.hcmute.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +15,28 @@ import java.util.Map;
 @RestController
 @Slf4j
 public class TokenProcessController {
-    @GetMapping("/token")
-    public Map<String, String> generateToken() {
-        log.info("TokenProcessController :: generateToken");
-        OAuth2User oAuth2User = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        log.info("Logged in user : {}", oAuth2User.getName());
-        Map<String, String> usersMap = new HashMap<>();
-        usersMap.put("Logged-in user", oAuth2User.getName());
+    private OAuth2AuthorizedClientService authorizedClientService;
 
-        log.info("Printing ID token attributes");
-        oAuth2User.getAttributes().keySet().forEach(key -> {
-            System.out.println(key + " = " + oAuth2User.getAttributes().get(key));
-        });
+    public TokenProcessController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
+    @GetMapping("/token")
+    public Map<String, String> generateToken(OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+        log.info("Access token: {}", accessToken);
+
+        OidcUser user = (OidcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("Id token: {}", user.getIdToken().getTokenValue());
+
+        Map<String, String> usersMap = new HashMap<>();
+        usersMap.put("access_token", accessToken);
+        usersMap.put("refresh_token", authorizedClient.getRefreshToken() != null ? authorizedClient.getRefreshToken().getTokenValue() : null);
+        usersMap.put("id_token", user.getIdToken().getTokenValue());
+        usersMap.put("token_type", authorizedClient.getAccessToken().getTokenType().getValue());
+        usersMap.put("token_expiry", authorizedClient.getAccessToken().getExpiresAt() != null ? String.valueOf(authorizedClient.getAccessToken().getExpiresAt()) : null);
         return usersMap;
     }
 }
