6e83ef26c03f95b5b70f7e719e12c645c13e13f9
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-14 16:08:37 +0700

    init registry server

---
+++ registry-server/src/main/java/edu/hcmute/RegistryServerApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServerApplication.class, args);
+    }
+
+}
