ecea983b5d4cf894979e2450339958478da34bfb
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-14 13:45:04 +0700

    add rest template

--- user-authnz-service/src/main/java/edu/hcmute/UserAuthnzServiceApplication.java
+++ user-authnz-service/src/main/java/edu/hcmute/UserAuthnzServiceApplication.java
@@ -2,6 +2,8 @@ package edu.hcmute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserAuthnzServiceApplication {
@@ -10,4 +12,8 @@ public class UserAuthnzServiceApplication {
         SpringApplication.run(UserAuthnzServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
