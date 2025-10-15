06d17f569803b4d1712cc98204f4f1c6f8534bea
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-15 16:34:44 +0700

    init identity gateway

---
+++ iden-gateway/src/main/java/edu/hcmute/idengateway/IdenGatewayApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute.idengateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdenGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdenGatewayApplication.class, args);
+    }
+
+}
