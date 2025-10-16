481960898f90b9c86bcb8d9b5f3aeca7c731432d
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-16 10:08:24 +0700

    enable discovery identity gateway

--- iden-gateway/src/main/java/edu/hcmute/IdenGatewayApplication.java
+++ iden-gateway/src/main/java/edu/hcmute/IdenGatewayApplication.java
@@ -1,9 +1,11 @@
-package edu.hcmute.idengateway;
+package edu.hcmute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IdenGatewayApplication {
 
     public static void main(String[] args) {
