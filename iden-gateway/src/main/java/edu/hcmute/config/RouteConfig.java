83a2643d8392d9f3eca302935a1e30a73631dec2
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-16 15:48:19 +0700

    add filter to configure RouteLocator

--- iden-gateway/src/main/java/edu/hcmute/config/RouteConfig.java
+++ iden-gateway/src/main/java/edu/hcmute/config/RouteConfig.java
@@ -1,17 +1,29 @@
 package edu.hcmute.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import java.time.Duration;
 
 @Configuration
 @Slf4j
 public class RouteConfig {
-//    @Bean
-//    public RouteLocator configureRoutes(RouteLocatorBuilder builder) {
-//        log.info("Configuring RouteLocator");
-//        return builder.routes()
-//                .route("userMgmt-getAllUsers-route", (route -> route.path("/users/**")
-//                        .uri("lb://USER-MGMT-SERVICE")))
+    @Bean
+    public RouteLocator configureRoutes(RouteLocatorBuilder builder) {
+        log.info("Configuring RouteLocator");
+        return builder.routes()
+                .route("userMgmt-getAllUsers-route", (route -> route
+                        .path("/picma/users/**")
+                        .filters(filter -> filter
+                                .addRequestHeader("userType", "agent")
+                                .rewritePath("/picma/users/?(?<segment>.*)", "/users/$\\{segment}")
+                                .addResponseHeader("userType", "agent")
+                                .localResponseCache(Duration.ofMinutes(3), DataSize.ofMegabytes(500)))
+                        .uri("lb://USER-MGMT-SERVICE")))
 //                .route("userMgmt-documents-route-01", (route -> route.path("/swagger-ui.html")
 //                        .uri("lb://USER-MGMT-SERVICE")))
 //                .route("userMgmt-documents-route-02", (route -> route.path("/swagger-ui/**")
@@ -20,6 +32,6 @@ public class RouteConfig {
 //                        .uri("lb://USER-MGMT-SERVICE")))
 //                .route("userAuthNZ-token-route", (route -> route.path("/token")
 //                        .uri("lb://USER-AUTHNZ-SERVICE")))
-//                .build();
-//    }
+                .build();
+    }
 }
