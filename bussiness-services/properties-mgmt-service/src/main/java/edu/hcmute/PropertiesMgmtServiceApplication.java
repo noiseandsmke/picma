3ca9578516cfa50ea11c4bff20db83459e4944d7
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-22 14:41:13 +0700

    init properties management service

---
+++ bussiness-services/properties-mgmt-service/src/main/java/edu/hcmute/PropertiesMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesMgmtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertiesMgmtServiceApplication.class, args);
+    }
+
+}
