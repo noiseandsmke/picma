15c6fe3bd52f306a7576a4bfdf1e97135501c8a4
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-24 00:08:59 +0700

    init PropertyQuoteService

---
+++ bussiness-services/property-quote-service/src/main/java/edu/hcmute/PropertyQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyQuoteServiceApplication.class, args);
+    }
+
+}
