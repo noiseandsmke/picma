9117b5ba6aa13dcad2436893a96e5c55fd92a351
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-23 23:28:31 +0700

    implement findPropertiesByZipCode

--- bussiness-services/property-mgmt-service/src/main/java/edu/hcmute/repo/PropertyInfoRepo.java
+++ bussiness-services/property-mgmt-service/src/main/java/edu/hcmute/repo/PropertyInfoRepo.java
@@ -2,6 +2,11 @@ package edu.hcmute.repo;
 
 import edu.hcmute.model.PropertyInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface PropertyInfoRepo extends MongoRepository<PropertyInfo, String> {
+    @Query("{'propertyAddress.zipCode': ?0}")
+    List<PropertyInfo> findPropertiesByZipCode(String zipcode);
 }
