aa9ab07df97dc2b4dc3eaae1c4f31be99d3ee810
noiseandsmke <emailaccanhbg11@gmail.com>
2025-10-23 15:52:49 +0700

    remove repo

--- bussiness-services/properties-mgmt-service/src/main/java/edu/hcmute/repo/PropertyAddressRepo.java
+++
@@ -1,7 +0,0 @@
-package edu.hcmute.repo;
-
-import edu.hcmute.model.PropertyAddress;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PropertyAddressRepo extends MongoRepository<PropertyAddress, String> {
-}
--- bussiness-services/properties-mgmt-service/src/main/java/edu/hcmute/repo/PropertyTypeRepo.java
+++
@@ -1,7 +0,0 @@
-package edu.hcmute.repo;
-
-import edu.hcmute.model.PropertyType;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PropertyTypeRepo extends MongoRepository<PropertyType, String> {
-}
