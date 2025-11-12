e218395ba9174bd235ac2f23dc3c7f175e7e2984
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-12 21:52:36 +0700

    add lead action and status enums

---
+++ business-services/property-lead-service/src/main/java/edu/hcmute/domain/LeadAction.java
@@ -0,0 +1,8 @@
+
+package edu.hcmute.domain;
+
+public enum LeadAction {
+    INTERESTED,
+    REJECTED,
+    ACCEPTED
+}
---
+++ business-services/property-lead-service/src/main/java/edu/hcmute/domain/LeadStatus.java
@@ -0,0 +1,10 @@
+package edu.hcmute.domain;
+
+public enum LeadStatus {
+    ACTIVE,
+    PENDING,
+    ASSIGNED,
+    ACCEPTED,
+    REJECTED,
+    EXPIRED
+}
