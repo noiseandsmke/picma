3a71b64dc72c084c02e5519d71d3e3c0355fb8d5
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-12 21:52:52 +0700

    add findByStatus

--- business-services/property-lead-service/src/main/java/edu/hcmute/repo/PropertyLeadRepo.java
+++ business-services/property-lead-service/src/main/java/edu/hcmute/repo/PropertyLeadRepo.java
@@ -3,5 +3,8 @@ package edu.hcmute.repo;
 import edu.hcmute.entity.PropertyLead;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropertyLeadRepo extends JpaRepository<PropertyLead, Integer> {
+    List<PropertyLead> findByStatus(String status);
 }
