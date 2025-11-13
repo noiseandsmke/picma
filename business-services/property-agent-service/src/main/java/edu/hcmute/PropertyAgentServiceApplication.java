e90151bc9d58aeaccfb7138b605c1ac4700d3298
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-13 10:34:23 +0700

    init property agent service

---
+++ business-services/property-agent-service/src/main/java/edu/hcmute/PropertyAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyAgentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyAgentServiceApplication.class, args);
+    }
+
+}
