151f445b9f58890fa17fafb6b8b02bb71bbc2acc
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-14 14:06:18 +0700

    update to fetch only the userId

--- business-services/property-agent-service/src/main/java/edu/hcmute/repo/UserAddressRepo.java
+++ business-services/property-agent-service/src/main/java/edu/hcmute/repo/UserAddressRepo.java
@@ -2,10 +2,12 @@ package edu.hcmute.repo;
 
 import edu.hcmute.entity.UserAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserAddressRepo extends JpaRepository<UserAddress, Integer> {
-    List<UserAddress> findByZipCode(@Param("zipCode") String zipCode);
+    @Query("SELECT ua.userId FROM UserAddress ua WHERE ua.zipCode = :zipCode")
+    List<String> findUserIdsByZipCode(@Param("zipCode") String zipCode);
 }
--- business-services/property-agent-service/src/main/java/edu/hcmute/service/PropertyAgentServiceImpl.java
+++ business-services/property-agent-service/src/main/java/edu/hcmute/service/PropertyAgentServiceImpl.java
@@ -3,13 +3,13 @@ package edu.hcmute.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.hcmute.config.PropertyInfoFeignClient;
-import edu.hcmute.entity.UserAddress;
 import edu.hcmute.repo.UserAddressRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -25,36 +25,35 @@ public class PropertyAgentServiceImpl implements PropertyAgentService {
         try {
             log.info("### Fetching agent within zip code: {} ###", propertyId);
             String propertyInfo = propertyInfoFeignClient.getPropertyInfoById(propertyId);
-            List<String> agentIds = null;
             JsonNode jsonObj = objectMapper.readTree(propertyInfo);
             log.info("~~> calling property-info-api to get address-info");
             log.info("~~> Property JSON response: {}", propertyInfo);
-            String zipCode = null;
-            if (jsonObj.has("propertyAddressDto")) {
-                JsonNode addressObj = jsonObj.get("propertyAddressDto");
-                if (addressObj.has("zipCode")) {
-                    zipCode = addressObj.get("zipCode").asText();
-                    log.info("ZipCode = {}", zipCode);
-                }
-            } else {
-                log.warn("propertyAddressDto field not found in property info response");
-            }
+
+            String zipCode = extractZipCode(jsonObj);
+
             if (StringUtils.hasText(zipCode)) {
-                agentIds = getUserIdsWithinZipCode(zipCode);
-                log.info("~~> agentIds = {}", agentIds);
+                List<String> agentIds = userAddressRepo.findUserIdsByZipCode(zipCode);
+                log.info("ZipCode = {}", zipCode);
+                log.info("### agentIds = {}", agentIds);
+                return agentIds;
             } else {
                 log.warn("zipCode is null or empty, cannot fetch agents");
+                return Collections.emptyList();
             }
-            // TODO: send notification to all agents with property lead
-            return agentIds;
         } catch (Exception e) {
-            log.error("### Error fetching agents within zip code ###", e);
+            log.error("Error fetching agents within zip code", e);
             throw new RuntimeException(e.getLocalizedMessage());
         }
     }
 
-    private List<String> getUserIdsWithinZipCode(String zipCode) {
-        List<UserAddress> userAddresses = userAddressRepo.findByZipCode(zipCode);
-        return userAddresses.stream().map(UserAddress::getUserId).toList();
+    private String extractZipCode(JsonNode jsonObj) {
+        if (jsonObj.has("propertyAddressDto")) {
+            JsonNode addressObj = jsonObj.get("propertyAddressDto");
+            if (addressObj.has("zipCode")) {
+                return addressObj.get("zipCode").asText();
+            }
+        }
+        log.warn("propertyAddressDto or zipCode field not found in property info response");
+        return null;
     }
 }
