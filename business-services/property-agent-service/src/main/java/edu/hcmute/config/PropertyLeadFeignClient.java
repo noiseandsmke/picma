1c191765e023bb3bdf2f2d63d2684424e41c6949
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-20 13:10:57 +0700

    implemented property lead FeignClient

---
+++ business-services/property-agent-service/src/main/java/edu/hcmute/config/PropertyLeadFeignClient.java
@@ -0,0 +1,12 @@
+package edu.hcmute.config;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(value = "property-lead-service", url = "${picma.properties.lead.base-uri}")
+public interface PropertyLeadFeignClient {
+    @PutMapping("/property-leads/{leadId}")
+    String updateLeadActionById(@PathVariable Integer leadId, @RequestHeader String leadStatus);
+}
