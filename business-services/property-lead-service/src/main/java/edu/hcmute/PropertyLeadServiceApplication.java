820080bbc0e9cc2fbb08d62ebc6446b3f5b9ade9
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-20 16:31:53 +0700

    enable FeignClients

--- business-services/property-lead-service/src/main/java/edu/hcmute/PropertyLeadServiceApplication.java
+++ business-services/property-lead-service/src/main/java/edu/hcmute/PropertyLeadServiceApplication.java
@@ -3,10 +3,12 @@ package edu.hcmute;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaAuditing
 public class PropertyLeadServiceApplication {
 
