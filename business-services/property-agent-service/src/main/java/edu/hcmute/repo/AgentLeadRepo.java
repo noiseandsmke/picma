1b6e2172e8f266fe14a96e46e5e5256b8e4aed54
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-20 13:12:07 +0700

    implemented agent lead repo

---
+++ business-services/property-agent-service/src/main/java/edu/hcmute/repo/AgentLeadRepo.java
@@ -0,0 +1,7 @@
+package edu.hcmute.repo;
+
+import edu.hcmute.entity.AgentLead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentLeadRepo extends JpaRepository<AgentLead, Integer> {
+}
