430acbab0fa7464e86f64dc569c2613fc3a56b4a
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-22 15:52:22 +0700

    migrate auditing to JaVers

--- business-services/property-lead-service/src/main/java/edu/hcmute/repo/PropertyLeadRepo.java
+++ business-services/property-lead-service/src/main/java/edu/hcmute/repo/PropertyLeadRepo.java
@@ -1,10 +1,12 @@
 package edu.hcmute.repo;
 
 import edu.hcmute.entity.PropertyLead;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@JaversSpringDataAuditable
 public interface PropertyLeadRepo extends JpaRepository<PropertyLead, Integer> {
     List<PropertyLead> findByStatus(String status);
 }
--- business-services/property-quote-service/src/main/java/edu/hcmute/repo/PropertyQuoteRepo.java
+++ business-services/property-quote-service/src/main/java/edu/hcmute/repo/PropertyQuoteRepo.java
@@ -1,7 +1,9 @@
 package edu.hcmute.repo;
 
 import edu.hcmute.entity.PropertyQuote;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@JaversSpringDataAuditable
 public interface PropertyQuoteRepo extends JpaRepository<PropertyQuote, Integer> {
 }
