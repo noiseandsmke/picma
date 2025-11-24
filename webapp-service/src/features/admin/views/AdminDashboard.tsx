43ccb3da9f58ec860bd396d0670f6292ab286890
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-24 18:27:58 +0700

    integrate admin dashboard to fetch data

--- webapp-service/src/features/admin/views/AdminDashboard.tsx
+++ webapp-service/src/features/admin/views/AdminDashboard.tsx
@@ -105,7 +105,7 @@ const AdminDashboard: React.FC = () => {
 
                     <Card className="bg-slate-900 border-slate-800 text-white">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium text-slate-400">Overdue / Alerts</CardTitle>
+                            <CardTitle className="text-sm font-medium text-slate-400">Expired</CardTitle>
                             <AlertTriangle className="h-4 w-4 text-amber-500"/>
                         </CardHeader>
                         <CardContent>
