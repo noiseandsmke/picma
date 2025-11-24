72a0e5cf11bd4a969d5e6daecf4a60dc5f447b7c
noiseandsmke <emailaccanhbg11@gmail.com>
2025-11-24 10:43:25 +0700

    implement login page with dark theme

--- webapp-service/src/layouts/AuthLayout.tsx
+++ webapp-service/src/layouts/AuthLayout.tsx
@@ -3,8 +3,8 @@ import {Outlet} from 'react-router-dom';
 
 const AuthLayout: React.FC = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-50">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-lg">
+        <div className="flex items-center justify-center min-h-screen bg-slate-950 p-4">
+            <div className="w-full max-w-md">
                 <Outlet/>
             </div>
         </div>
