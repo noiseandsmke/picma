2018e174cb3a306e95ff06e11f325d5e43d1cb35
noiseandsmke <emailaccanhbg11@gmail.com>
2025-12-03 19:03:41 +0700

    refactored iden-gateway to a stateless architecture

--- infrastructure-services/iden-gateway/src/main/java/edu/hcmute/config/SecurityConfig.java
+++ infrastructure-services/iden-gateway/src/main/java/edu/hcmute/config/SecurityConfig.java
@@ -3,29 +3,72 @@ package edu.hcmute.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
+@EnableWebFluxSecurity
 @Slf4j
 public class SecurityConfig {
+
     @Bean
-    public SecurityWebFilterChain configureResourceServer(ServerHttpSecurity httpSecurity) {
-        log.info("### Configure Resource Server ###");
-        return httpSecurity
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        log.info("### Configuring Security Web Filter Chain ###");
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .authorizeExchange(exchange -> exchange
+                .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/auth/**").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/login/**").permitAll()
-                        .pathMatchers(HttpMethod.POST, "/login/**").permitAll()
-                        .pathMatchers("/fallback").permitAll()
-                        .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/**").permitAll()
-                        .anyExchange().authenticated())
+                        .pathMatchers(HttpMethod.GET, "/picma/properties/**").permitAll()
+                        .pathMatchers("/picma/admin/**").hasRole("ADMIN")
+                        .pathMatchers("/picma/agent/**").hasRole("AGENT")
+                        .pathMatchers("/picma/owner/**").hasRole("OWNER")
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())).build();
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
+    static class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+        @Override
+        public Collection<GrantedAuthority> convert(Jwt jwt) {
+            final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+            if (realmAccess == null || realmAccess.isEmpty()) {
+                return Collections.emptyList();
+            }
+            return ((List<String>) realmAccess.get("roles")).stream()
+                    .map(roleName -> "ROLE_" + roleName)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
     }
 }
