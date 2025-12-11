afac80c15cef506ad521843ae0d12772a83bcf4e
noiseandsmke <emailaccanhbg11@gmail.com>
2025-12-11 17:29:39 +0700

    init deep research service

---
+++ business-services/deep-research-service/src/main/java/edu/hcmute/DeepResearchServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.hcmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepResearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepResearchServiceApplication.class, args);
+	}
+
+}
