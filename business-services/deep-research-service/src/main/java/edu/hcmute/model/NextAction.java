5f9f71cc82e0ac3ad0a1680916823906cce25412
noiseandsmke <emailaccanhbg11@gmail.com>
2025-12-12 09:00:38 +0700

    removed urls due to not using scrawl

--- business-services/deep-research-service/src/main/java/edu/hcmute/model/NextAction.java
+++ business-services/deep-research-service/src/main/java/edu/hcmute/model/NextAction.java
@@ -1,12 +1,9 @@
 package edu.hcmute.model;
 
-import java.util.List;
-
 public record NextAction(
         ActionType type,
         String reasoning,
         String query,
-        List<String> urls,
         String answer
 ) {
     public enum ActionType {
