142c2bcecb16340179ec57c30633ccaa6ac8f841
noiseandsmke <emailaccanhbg11@gmail.com>
2025-12-14 09:55:56 +0700

    deleted PropertyAddressDto and replaced by internal LocationDto

--- business-services/property-agent-service/src/main/java/edu/hcmute/dto/PropertyAddressDto.java
+++
@@ -1,9 +0,0 @@
-package edu.hcmute.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record PropertyAddressDto(
-        String zipCode
-) {
-}
