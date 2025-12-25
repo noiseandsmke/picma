a141c7982bf9740749f51473734f51da2da6b3a0
noiseandsmke <emailaccanhbg11@gmail.com>
2025-12-25 19:34:11 +0700

    removed ROUTES constant and update default API_URL value

--- webapp-service/src/config/env.ts
+++ webapp-service/src/config/env.ts
@@ -1,14 +1,5 @@
+// @ts-ignore
 export const ENV = {
-    API_URL: import.meta.env.VITE_API_URL || 'http:
+    API_URL: import.meta.env.VITE_API_URL || 'http://localhost:6060',
     CLIENT_ID: import.meta.env.VITE_CLIENT_ID || 'webapp-client',
 };
-export const ROUTES = {
-    HOME: '/',
-    SIGNIN: '/signin',
-    SIGNUP: '/signup',
-    DASHBOARD: {
-        ADMIN: '/admin/dashboard',
-        AGENT: '/agent/dashboard',
-        OWNER: '/owner/dashboard',
-    },
-} as const;
